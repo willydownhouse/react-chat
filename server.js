5c55b35250f3a404513f6c14807a6b21a0b06378
willydownhouse <ville.l.alatalo@gmail.com>
2022-04-21 10:38:40 +0200

    prod1.1

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import path from 'path';
+import favicon from 'express-favicon';
 const __dirname = path.resolve();
 
 const app = express();
 
-const PORT = 3001;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 8080;
 
 app.get('/ping', (_, res) => {
   res.send('pong');
 });
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -17,3 +21,5 @@ app.get('/', function (req, res) {
 });
 
 app.listen(PORT, () => console.log(`App listening port ${PORT}`));
+
+//https://powerful-depths-16588.herokuapp.com/
